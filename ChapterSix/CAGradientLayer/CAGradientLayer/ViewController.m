32b62e0426316686f6ac5c2e3eb80730c1c53d31
Insofan <282132315@qq.com>
2017-04-20 12:01:44 +0800

    [add](gradientLayer)添加渐变颜色

--- ChapterSix/CAGradientLayer/CAGradientLayer/ViewController.m
+++ ChapterSix/CAGradientLayer/CAGradientLayer/ViewController.m
@@ -9,6 +9,7 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIView *containerView;
 
 @end
 
@@ -17,6 +18,25 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    //create gradient layer and add it to our container view
+    CAGradientLayer *gradientLayer = [CAGradientLayer layer];
+    gradientLayer.frame = self.containerView.bounds;
+    [self.containerView.layer addSublayer:gradientLayer];
+    
+    /*双色渐变
+    //set gradient colors
+    gradientLayer.colors = @[(__bridge id)[UIColor redColor].CGColor, (__bridge id)[UIColor blueColor].CGColor];
+    //set gradient start and end points
+    gradientLayer.startPoint = CGPointMake(0, 0);
+    gradientLayer.endPoint = CGPointMake(1, 1);
+     */
+    //set gradient colors
+    gradientLayer.colors = @[(__bridge id)[UIColor redColor].CGColor, (__bridge id)[UIColor yellowColor].CGColor, (__bridge id)[UIColor greenColor].CGColor];
+    //set locations
+    gradientLayer.locations = @[@0.0, @0.25, @0.5];
+    //set gradient start and end points
+    gradientLayer.startPoint = CGPointMake(0, 0);
+    gradientLayer.endPoint = CGPointMake(1, 1);
 }
 
 
